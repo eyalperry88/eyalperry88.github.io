ba0f743435cb8245fcb1d32d973dc62d567dee04
Eyal Perry <eyal.perry88@gmail.com>
2019-04-19 00:32:07 -0500

    changed random values

--- dormio/js/script.js
+++ dormio/js/script.js
@@ -69,9 +69,9 @@ function handleBatteryLevelChanged(event) {
 
   //newData = new Uint32Array(data);
   oldHr = hr ;
-  flex = val + 0 + Math.floor(Math.random() * 10); //newData[0];
-  hr = val + 100 + Math.floor(Math.random() * 10); //newData[1];
-  eda = val + 200 + Math.floor(Math.random() * 10); //newData[2];
+  flex = val + 200 + Math.floor(Math.random() * 50); //newData[0];
+  hr = val + 100 + Math.floor(Math.random() * 50); //newData[1];
+  eda = val + 0 + Math.floor(Math.random() * 50); //newData[2];
   buffer.push(hr);
   if (buffer.length > 600) {
     buffer.shift();
